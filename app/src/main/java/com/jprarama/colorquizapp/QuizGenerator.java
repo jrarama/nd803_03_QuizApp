a271c977ad83b8e689f25f1ac770930d23b8cb33
jrarama <joshuararama@gmail.com>
2016-07-02 17:28:32 +0800

    feat: Added header logo

--- app/src/main/java/com/jprarama/colorquizapp/QuizGenerator.java
+++ app/src/main/java/com/jprarama/colorquizapp/QuizGenerator.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import static com.jprarama.colorquizapp.entity.ColorCombination.CombinationType.ADDITIVE;
 import static com.jprarama.colorquizapp.entity.ColorCombination.CombinationType.PRIMARY;
